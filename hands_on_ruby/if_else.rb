6011f5c0256bbcc8dda8b20246fcf144cb27f467
harish khileri <harishkhileri98@gmail.com>
2025-10-08 15:53:11 +0530

    convert user input string to number and then apply conditioin

--- hands_on_ruby/if_else.rb
+++ hands_on_ruby/if_else.rb
@@ -1,5 +1,6 @@
 puts "In this file are are check how to work if else in ruby"
-age = gets.chomp
+num = gets.chomp
+age = num.to_i
 if age >= 18
     puts "Your age is #{age} and you can vote"
 elsif age < 18
